04f88d79a2523184d73ea6c9be2954883555f733
Wumberly <67195519+Wumberly@users.noreply.github.com>
2021-11-15 10:58:16 +0100

    Update isPalindrome.py

--- day1/isPalindrome.py
+++ day1/isPalindrome.py
@@ -1,4 +1,17 @@
-def isPalindrome(string):
-	return False;
+def wordflipper(input):
+    a = len(input)
+    i = 0
+    output = ''
+    while i < a:
+        b = a - i
+        output += input[b - 1]
+        i += 1
+    return output
 
-print(isPalindrome(input('Enter input:\n')))
+def palindromecheck(input):
+    flipped = wordflipper(input)
+
+    if flipped == input:
+        print("Yup, it's a Palindrome")
+    else:
+        print("Nope, not a palindrome")
